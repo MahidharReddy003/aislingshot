a626a236e4b5902456c351982f96f442b2cdfb4a
MAHIDHAR REDDY <mahidharreddy963@gmail.com>
2026-02-19 05:48:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6271909187-105a3",
+  "appId": "1:1062392916996:web:2ea23c0560fdedd6f45951",
+  "apiKey": "AIzaSyBYLnKbjgbwIilfuVZpGyC3c1zqvqFg7_U",
+  "authDomain": "studio-6271909187-105a3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1062392916996"
+};
